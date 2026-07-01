6c6c33991a0292addb89339d118262fbe35cd5c8
gcat332 <kasira.cat@gmail.com>
2026-07-01 21:14:53 +0700

    Scaffold PADDictionary iOS app via Xcode
    SwiftUI app + unit test target, iOS 17 minimum deployment. Verified
    end-to-end with xcodebuild test on iPhone 17 Simulator.

---
+++ ios/PADDictionary/PADDictionary/PADDictionaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PADDictionaryApp.swift
+//  PADDictionary
+//
+//  Created by gust on 1/7/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct PADDictionaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/PADDictionary/PADDictionaryTests/PADDictionaryTests.swift
@@ -0,0 +1,18 @@
+//
+//  PADDictionaryTests.swift
+//  PADDictionaryTests
+//
+//  Created by gust on 1/7/2569 BE.
+//
+
+import Testing
+
+struct PADDictionaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
